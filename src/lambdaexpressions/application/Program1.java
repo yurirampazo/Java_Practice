a376171fb12b6135dee2955751bcd9c4ee3f06eb
Yuri <yuri.rampazo@outlook.com>
2021-07-31 01:12:09 -0300

    Lambda Expressions: Comparator without foreign class

--- src/lambdaexpressions/application/Program1.java
+++ src/lambdaexpressions/application/Program1.java
@@ -16,8 +16,14 @@ public class Program1 {
         list.add(new Product("TV", 900.00));
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        };
 
-        list.sort(new MyComparator());
+        list.sort(comp);
 
         for (Product p : list) {
             System.out.println(p);
