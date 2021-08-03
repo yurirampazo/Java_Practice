7a17ac91e46f33ebcbc5c9aa4111aa6cb452cee0
Yuri <yuri.rampazo@outlook.com>
2021-08-03 11:54:04 -0300

    Lambda Expressions: Function with declared and inline lambda expression.

--- src/lambdaexpressions/application/FunctionExample.java
+++ src/lambdaexpressions/application/FunctionExample.java
@@ -6,6 +6,7 @@ import lambdaexpressions.model.util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class FunctionExample {
@@ -19,8 +20,21 @@ public class FunctionExample {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        //Static Reference Method
        // List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
-        List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+
+
+        //Non-static reference method
+        //List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+
+
+        //Declared Lambda
+        //Function<Product, String> function = f -> f.getName().toUpperCase();
+       // List<String> names = list.stream().map(function).collect(Collectors.toList());
+
+        //Inline Lambda -> Prettier
+        List<String> names = list.stream().map(f -> f.getName().toUpperCase()).collect(Collectors.toList());
+
 
         names.forEach(System.out::println);
 
