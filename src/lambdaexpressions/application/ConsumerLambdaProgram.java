2aebced3f1f51cd1c08675b4a0561be0f5dc22a5
Yuri <yuri.rampazo@outlook.com>
2021-08-03 01:01:52 -0300

    Lambda Expressions: Consumer with declared lambda expression

--- src/lambdaexpressions/application/ConsumerLambdaProgram.java
+++ src/lambdaexpressions/application/ConsumerLambdaProgram.java
@@ -1,15 +1,17 @@
 package lambdaexpressions.application;
 
 import lambdaexpressions.model.entities.Product;
-import lambdaexpressions.model.service.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
 
-public class ConsumerExample {
+public class ConsumerLambdaProgram {
     public static void main(String[] args) {
+
+        //Declared Lambda
+
         Locale.setDefault(Locale.US);
         List<Product> list = new ArrayList<>();
 
@@ -18,8 +20,13 @@ public class ConsumerExample {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+//        Consumer<Product> cons = c -> {
+//            c.setPrice(c.getPrice() * factor);
+//        };
+        Consumer<Product> cons = c -> c.setPrice(c.getPrice() * factor);
 
+        list.forEach(cons);
         list.forEach(System.out::println);
     }
 }
