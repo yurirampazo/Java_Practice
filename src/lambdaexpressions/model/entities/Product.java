c552cc2825b3eba2abaa67b0cb3b6d72cea3da81
Yuri <yuri.rampazo@outlook.com>
2021-08-03 00:48:00 -0300

    Lambda Expressions: Consumer with non-static method reference

--- src/lambdaexpressions/model/entities/Product.java
+++ src/lambdaexpressions/model/entities/Product.java
@@ -38,6 +38,12 @@ public class Product {
         p.setPrice(p.getPrice() * 1.1);
     }
 
+
+    public void nonStaticPriceUpdate() {
+        //price = price * 1.1;
+        setPrice(getPrice() * 1.1);
+    }
+
     @Override
     public String toString(){
         return name + ", " + String.format("%.2f", price);
